4896617a2975f2c1e21347e8160af15223e9aa0e
Rashid <valentaine1234@gmail.com>
2026-02-25 14:45:12 +0300

    country enums

---
+++ enums/country.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class CountryEnum(str, Enum):
+    RUSSIA = "15000203"
+    BELARUS = "15001048"
+    CHINA = "15000170"
+    KYRGYZSTAN = "15000126"
+    TURKEY = "15000257"
+    UK = "15000267"
+    VIETNAM = "15000223"
+    ITALY = "15000118"
+    FINLAND = "15000084"
