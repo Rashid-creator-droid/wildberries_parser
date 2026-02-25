2786f5de70ba276d39cd270927bd702c5ec91b3b
Rashid <valentaine1234@gmail.com>
2026-02-25 19:56:44 +0300

    main func

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-from core.json_loader import DataLoader
-from utils.data_save_service import save_products_to_excel
+from core.data_loader import DataLoader
+from core.exporter import Exporter
 from services.parser import Parser
 
 import asyncio
 
 async def main():
-    config = DataLoader().get_config()     
+    config = DataLoader().get_config()
     parser = Parser(config)
-    items = await parser.fetch_all_products(limit_per_page=10, limit_pages=1, use_filters=False)
-    print(f"загружено {len(items)} товаров")
-    save_products_to_excel(items, "wildberries_products.xlsx")
+    exporter = Exporter("wildberries_products.xlsx")
+    batch: list[dict] = []
+    total_saved = 0
+
+    try:
+        async for prod in parser.iter_products(
+            limit_per_page=87, limit_pages=1, use_filters=False
+        ):
+            total_saved += 1
+            batch.append(prod)
+            print(f"Processing product {prod['article']}: {prod['name']}")
+
+            if len(batch) >= 50:
+                await exporter.append_batch_async(batch)
+        if batch:
+            await exporter.append_batch_async(batch)
+
+    finally:
+        await parser.close()
+
+    print(f"загружено {total_saved} товаров")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
